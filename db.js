1cb6d6174e5b90bbd23102243af9910c5c2c5fad
Pedro <pedrodanilo48@gmail.com>
2026-03-27 16:01:33 -0300

    Criando setup

--- db.js
+++ db.js
@@ -19,4 +19,22 @@ async function connect(){
     return pool.connect();
 }
 
-connect();
+async function selectCustomer(){
+    const client = await connect();
+    const res = await client.query("SELECT * FROM clientes");
+    return res.rows;
+}
+
+async function setupDatabase(){
+    const client = await connect();
+    const query = `CREATE TABLE IF NOT EXISTS  clients (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL);`;
+
+    await client.query(query);
+    client.release();
+    console.log("Estrutura da tabela criada");
+}
+
+module.exports = { connect, selectCustomer, setupDatabase };
--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.get("/", (req, res) => {
     res.json({ message: "Olá, mundo!" });
 });
 
-app.listen(port);
+app.listen(port, async () => {
+    console.log(`Servidor rodando na porta ${port}`);
 
-console.log(`Servidor rodando na porta ${port}`);
+    try {
+        await db.setupDatabase();
+        console.log("Banco de dados configurado");
+    } catch (error) {
+        console.error("Erro ao configurar o banco de dados:", error);
+    }
+})
